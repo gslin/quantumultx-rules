cd46e7aa537e067e09c67ccb2e1580642a8afe1f
Gea-Suan Lin <gslin@gslin.org>
2024-06-05 10:59:42 +0800

    Refactor to use `<<<`.

--- sync-easylist.sh
+++ sync-easylist.sh
@@ -3,5 +3,10 @@
 EASYLIST_URL=https://easylist.to/easylist/easylist.txt
 EASYLIST_DATA=$(curl -s ${EASYLIST_URL})
 
-echo -n -- "${EASYLIST_DATA}" | grep -E '^\|\|' | grep -v -E '(\$|\^|\?)' | cut -c 3- | sed -E 's|/.*||' | sort -u | awk '{print $1 "\n*." $1}' | xargs | awk '{print "hostname = " $0}'
-echo -n -- "${EASYLIST_DATA}" | grep -E '^\|\|' | grep -v -E '(\$|\^|\?)' | cut -c 3- | sed -E -e 's|/|\\/|g' -e 's|\.|\\.|g' | awk '{print "^https?:\\/\\/.*\\." $1 " url reject"}'
+(
+    grep -E '^\|\|' | grep -v -E '(\$|\^|\?)' | cut -c 3- | sed -E 's|/.*||' | sort -u | awk '{print $1 "\n*." $1}' | xargs | awk '{print "hostname = " $0}'
+) <<< "${EASYLIST_DATA}"
+
+(
+    grep -E '^\|\|' | grep -v -E '(\$|\^|\?)' | cut -c 3- | sed -E -e 's|/|\\/|g' -e 's|\.|\\.|g' | awk '{print "^https?:\\/\\/.*\\." $1 " url reject"}'
+) <<< "${EASYLIST_DATA}"
