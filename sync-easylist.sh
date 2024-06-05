247b007535e289bbf196d70eaf058ab29fe96fea
Gea-Suan Lin <gslin@gslin.org>
2024-06-05 11:00:24 +0800

    Refine comments.

--- sync-easylist.sh
+++ sync-easylist.sh
@@ -3,10 +3,12 @@
 EASYLIST_URL=https://easylist.to/easylist/easylist.txt
 EASYLIST_DATA=$(curl -s ${EASYLIST_URL})
 
+# Output MITM hostnames.
 (
     grep -E '^\|\|' | grep -v -E '(\$|\^|\?)' | cut -c 3- | sed -E 's|/.*||' | sort -u | awk '{print $1 "\n*." $1}' | xargs | awk '{print "hostname = " $0}'
 ) <<< "${EASYLIST_DATA}"
 
+# Output rules.
 (
     grep -E '^\|\|' | grep -v -E '(\$|\^|\?)' | cut -c 3- | sed -E -e 's|/|\\/|g' -e 's|\.|\\.|g' | awk '{print "^https?:\\/\\/.*\\." $1 " url reject"}'
 ) <<< "${EASYLIST_DATA}"
