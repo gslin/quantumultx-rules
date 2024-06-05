de006e400c38a7cbe7cd609f57687c705c589acc
Gea-Suan Lin <gslin@gslin.org>
2024-06-05 10:42:29 +0800

    Refactor.

--- sync-jkgtw-line.sh
+++ sync-jkgtw-line.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 JKGTW_LINEP_URL=https://raw.githubusercontent.com/jkgtw/Surge/master/Modules/LINE-ADs.sgmodule
-JKGTW_LINE_DATA=$(curl -s ${JKGTW_LINEP_URL})
+JKGTW_LINE_DATA=$(curl -s ${JKGTW_LINEP_URL} | grep -E '^URL-REGEX,')
 
 echo '# Line'
 echo -n 'hostname = '
-grep -E '^URL-REGEX,' <<< "${JKGTW_LINE_DATA}" | sed -E -e 's/^URL-REGEX,//' -e 's/,REJECT(-DROP)?$//' -e 's@^\^https:\\/\\/@@' -e 's@\\/.*@@' -e 's/\\//g' | env -i sort -u | xargs
-grep -E '^URL-REGEX,' <<< "${JKGTW_LINE_DATA}" | sed -E -e 's/^URL-REGEX,//' -e 's/,REJECT(-DROP)?$//' -e 's/$/ url reject/' | env -i sort
+
+(
+    sed -E \
+        -e 's/^URL-REGEX,//' \
+        -e 's/,REJECT(-DROP)?$//' \
+        -e 's@^\^https:\\/\\/@@' \
+        -e 's@\\/.*@@' \
+        -e 's/\\//g' | \
+        env -i sort -u | xargs
+) <<< "${JKGTW_LINE_DATA}"
+
+(
+    sed -E \
+        -e 's/^URL-REGEX,//' \
+        -e 's/,REJECT(-DROP)?$//' \
+        -e 's/$/ url reject/' | \
+        env -i sort
+) <<< "${JKGTW_LINE_DATA}"
